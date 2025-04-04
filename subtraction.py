f5691e157366259d27f81ec61add8924844ff53b
fanofpspk <vamshichandra8587@gmail.com>
2025-04-04 14:37:34 +0530

    updated subtraction.py with changes

--- subtraction.py
+++ subtraction.py
@@ -2,4 +2,12 @@
 a = 10
 b = 5
 result = a - b
-print("Subtraction Result:", result)  # Print output
+result1=a+b
+result2=a/b
+result3=a//b
+result4=a**b
+print("Subtraction Result:", result)
+print("Addition Result:", result1)
+print("Division Result:", result2)
+print("Floor division Result:", result3)
+print("Exponentiation Result:", result4)
